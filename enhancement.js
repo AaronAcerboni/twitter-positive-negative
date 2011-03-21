4799408874353eae869b9f73f3da30dc67b22887
Aaron Acerboni <a.acerboni@gmail.com>
2011-03-21 13:36:45 +0000

    part of example page

---
+++ enhancement.js
@@ -0,0 +1,31 @@
+//to be done at a later date, not really fundamental
+
+$(document).ready(function(){
+	
+	/*Initialise listeners*/
+	$("div.section form").submit(
+		function(){
+			query();
+			return false;
+		}
+	);
+	
+	/*Functions*/
+	function query(){
+		
+		var v = $("form").serialize();
+		
+		$.get(
+			"php/routine.php",
+			v,
+			function(data){
+				parseData(data);
+			}
+			);
+	}
+	
+	function parseData(data){
+		$('#graph').html("<h1>Optimism</h1>"+data);
+	}
+	
+});
